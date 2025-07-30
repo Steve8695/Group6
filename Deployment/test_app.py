ebeaf01a4e8b2b5a6ec717a181819596394a063a
Steve8695 <51990522+Steve8695@users.noreply.github.com>
2025-07-30 16:09:35 -0400

    Update test_app.py

--- Deployment/test_app.py
+++ Deployment/test_app.py
@@ -1,5 +1,8 @@
-# Deployment/test_app.py
-from Deployment import app
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)))
 
-def test_dummy():
-    assert hasattr(app, '__file__')
+import app
+
+def test_basic_route():
+    assert True  # Replace with real test if needed
